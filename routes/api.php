69fa796f15a9610805047f657bdcfe490f2df321
Ali <aliaslani1727@gmail.com>
2024-05-09 00:50:05 +0430

    refactor: (refactoring Api Docs)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\Admin\Berand\BerandController;
 use App\Http\Controllers\Api\Admin\Category\CategoryController;
 use App\Http\Controllers\Api\Admin\Color\ColorController;
+use App\Http\Controllers\Api\Admin\Locality\CityController;
+use App\Http\Controllers\Api\Admin\Locality\StateController;
 use App\Http\Controllers\Api\Admin\Product\ProductController;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Home\User\UserControllerapi;
@@ -57,6 +59,26 @@ Route::prefix('admin')->middleware('auth:sanctum','role:Admin')->group(function 
         Route::delete('destroy', [ColorController::class, 'destroy'])->middleware('role:Delete');
         Route::put('restore', [ColorController::class, 'restore'])->middleware('role:Update');
     });
+    Route::prefix('State')->group(function () {
+        Route::get('index', [StateController::class, 'index'])->middleware('role:Read');
+        Route::get('create', [StateController::class, 'create'])->middleware('role:Create');
+        Route::post('store', [StateController::class, 'store'])->middleware('role:Create');
+        Route::get('show', [StateController::class, 'show'])->middleware('role:Read');
+        Route::get('edit', [StateController::class, 'edit'])->middleware('role:Update');
+        Route::post('update', [StateController::class, 'update'])->middleware('role:Update');
+        Route::delete('destroy', [StateController::class, 'destroy'])->middleware('role:Delete');
+        Route::put('restore', [StateController::class, 'restore'])->middleware('role:Update');
+    });
+    Route::prefix('City')->group(function () {
+        Route::get('index', [CityController::class, 'index'])->middleware('role:Read');
+        Route::get('create', [CityController::class, 'create'])->middleware('role:Create');
+        Route::post('store', [CityController::class, 'store'])->middleware('role:Create');
+        Route::get('show', [CityController::class, 'show'])->middleware('role:Read');
+        Route::get('edit', [CityController::class, 'edit'])->middleware('role:Update');
+        Route::post('update', [CityController::class, 'update'])->middleware('role:Update');
+        Route::delete('destroy', [CityController::class, 'destroy'])->middleware('role:Delete');
+        Route::put('restore', [CityController::class, 'restore'])->middleware('role:Update');
+    });
     Route::prefix('Product')->group(function () {
         Route::get('index', [ProductController::class, 'index'])->middleware('role:Read');
         Route::get('create', [ProductController::class, 'create'])->middleware('role:Create');
@@ -67,7 +89,9 @@ Route::prefix('admin')->middleware('auth:sanctum','role:Admin')->group(function 
         Route::delete('destroy', [ProductController::class, 'destroy'])->middleware('role:Delete');
         Route::put('restore', [ProductController::class, 'restore'])->middleware('role:Update');
         Route::post('add/Color/Price', [ProductController::class, 'addProductColorPrice'])->middleware('role:Create');
+        Route::post('update/Color/Price', [ProductController::class, 'updateProductColorPrice'])->middleware('role:Create');
         Route::post('add/Detial', [ProductController::class, 'addProductDetial'])->middleware('role:Create');
+        Route::post('update/Detial', [ProductController::class, 'updateProductDetial'])->middleware('role:Create');
         Route::post('add/image', [ProductController::class, 'addProductImage'])->middleware('role:Create');
         Route::delete('remove/image', [ProductController::class, 'removeProductImage'])->middleware('role:Delete');
     });
