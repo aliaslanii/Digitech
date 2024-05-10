f77566781bb9a1d135054b3a50fab91e7db502d1
Ali <aliaslani1727@gmail.com>
2024-05-10 23:54:04 +0430

    refactor: (refactoring swagger Response)

--- app/Http/Controllers/Api/Home/Product/ProductController.php
+++ app/Http/Controllers/Api/Home/Product/ProductController.php
@@ -21,8 +21,15 @@ class ProductController extends Controller
      *     path="/api/Product/index",
      *     summary="index Products",
      *     tags={"Product Home"},
-     *     @OA\Response(response="200", description="Test Product Created successfully"),
-     *     @OA\Response(response="403", description="Validation errors")
+     *     @OA\Response(
+     *          response=200,
+     *          description="Data create successfully",
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid data",
+     *      ),
+     * 
      * )
      */
     public function index()
@@ -43,33 +50,39 @@ class ProductController extends Controller
             ], 500);
         }
     }
-     /**
+    /**
      * @OA\Get(
      *     path="/api/Product/Show",
      *     summary="index Products",
      *     tags={"Product Home"},
      *     @OA\Parameter(
-     *         name="id",
+     *         name="dtp",
      *         in="query",
-     *         description="id product",
+     *         description="dtp product",
      *         required=true,
      *         @OA\Schema(type="integer")
      *     ),
-     *     @OA\Response(response="200", description="Test Product Created successfully"),
-     *     @OA\Response(response="403", description="Validation errors")
+     *     @OA\Response(
+     *          response=200,
+     *          description="Data create successfully",
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid data",
+     *      ),
      * )
      */
     public function showProduct(Request $request)
     {
         try {
-            $Product = Product::find($request->id);
+            $Product = Product::where('dtp', $request->dtp)->first();
             $Product->incrementViews();
             $Response = [];
             $ProductData = [
                 "name" => $Product->name,
                 "description" => $Product->description,
                 "stock quantity" => $Product->stock_quantity,
-                "specific" => $Product->specific, 
+                "specific" => $Product->specific,
                 "main photo" => $Product->photo_path,
                 "additional photo" => $this->HomeProduct->getImages($Product),
                 "Category" => $Product->Category->name,
